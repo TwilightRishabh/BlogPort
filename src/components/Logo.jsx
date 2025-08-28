d7fddd42557fc5b0bb6ecd18c595b6f625009ddf
twilightrishabh <rishabhgupta88k@gmail.com>
2025-08-28 21:27:41 +0530

    change logo to BlogPort

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Logo({width='100px'}) {
-  return ( <div className=' text-blue-600 hover:text-blue-700'>Logo</div> )
+  return ( <div className=' text-blue-600 hover:text-blue-700'>BlogPort</div> )
 }
 
 export default Logo
